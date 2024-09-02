c51d58c8b62d2c81e24b59cb430c3b2db1afab13
unknown <bilaldeveloper900@gamil.com>
2024-09-02 18:13:31 +0500

    correct the login error

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,6 @@ const { createUser, loginUser  } = require('../controllers/userController');
 var router = express.Router();
 
 router.post('/register', createUser);
-router.get('/login', loginUser);
+router.post('/login', loginUser);
 
 module.exports = router;
