beff54d9cdbf1d6a5892c3c570ebe774ae024aa9
unknown <bilaldeveloper900@gamil.com>
2024-09-04 01:24:26 +0500

    add controlers function in the routes

--- routes/subCategory.js
+++ routes/subCategory.js
@@ -1,12 +1,12 @@
 var express = require('express');
 const { verifyToken } = require('../middleware/authmiddleware');
-const { createSubCategory, getSubCategory } = require('../controllers/dashboard/subCategoryController');
+const { createSubCategory, getSubCategory, getSubCategoryById, updateSubCategory, deleteSubCategory } = require('../controllers/dashboard/subCategoryController');
 var router = express.Router();
 
 router.post('/', verifyToken, createSubCategory)
 router.get('/', verifyToken, getSubCategory)
-router.get('/:id', verifyToken)
-router.put('/:id', verifyToken)
-router.delete('/:id', verifyToken)
+router.get('/:id', verifyToken, getSubCategoryById)
+router.put('/:id', verifyToken, updateSubCategory)
+router.delete('/:id', verifyToken, deleteSubCategory)
 
 module.exports = router;
